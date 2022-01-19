9165a993b96d789ea49d2f621baa4b94258c7af1
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-01-19 11:26:02 +0100

    feat: first implementation of data grid widget

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import DataGridWidget from './components/manage/Widgets/DataGridWidget.jsx';
+export { DataGridWidget };
+
 const applyConfig = (config) => {
+  config.widgets.widget.data_grid = DataGridWidget;
   return config;
 };
 
