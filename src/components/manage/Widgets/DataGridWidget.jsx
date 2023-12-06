17701f30f8b49f5d013537da25b9e823eef4bcf9
Piero Nicolli <piero.nicolli@redturtle.it>
2023-12-06 13:24:15 +0100

    feat: widget is now sortable

--- src/components/manage/Widgets/DataGridWidget.jsx
+++ src/components/manage/Widgets/DataGridWidget.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
+import cx from 'classnames';
+import move from 'lodash-move';
 import { FormFieldWrapper, Field } from '@plone/volto/components';
-import { Grid, Button } from 'semantic-ui-react';
+import DragDropList from '@plone/volto/components/manage/DragDropList/DragDropList';
+import { Grid, Button, Form, Icon } from 'semantic-ui-react';
 import './data-grid-widget.less';
+import dragSVG from '@plone/volto/icons/drag.svg';
 
 const messages = defineMessages({
   addTerm: {
@@ -29,7 +33,21 @@ const defaultSchema = {
 
 const DataGridWidget = (props) => {
   const intl = useIntl();
-  const { value, id, onChange, items = defaultSchema, widgetOptions } = props; //, required, title, description
+  const {
+    value,
+    id,
+    onChange,
+    items = defaultSchema,
+    widgetOptions,
+    required = false,
+    error = [],
+    multilingual_options,
+    description = null,
+    className,
+    fieldSet,
+    title,
+    noForInFieldLabel,
+  } = props; //, required, title, description
   const schema = items;
   const [values, setValues] = useState([]);
   const [defaultItem, setDefaultItem] = useState({});
@@ -71,87 +89,206 @@ const DataGridWidget = (props) => {
     handleChangeConfiguration(newValues);
   };
 
+  const moveTerm = (source, destination) => {
+    const newValues = move([...values], source, destination);
+
+    handleChangeConfiguration(newValues);
+  };
+
   const allowDelete = widgetOptions?.allow_delete ?? true;
   const allowAppend = widgetOptions?.allow_insert ?? true;
-  // TODO: allow_reorder, auto_append
+  const allowReorder = widgetOptions?.allow_insert ?? true;
+  // TODO: auto_append
 
   return (
-    <FormFieldWrapper {...props}>
-      <div className="data-grid-widget">
-        {schema && (
-          <Grid verticalAlign="middle" stackable columns="equal">
-            {values?.map((term, index) => (
-              <Grid.Row key={index}>
-                {schema.fieldsets.map((fieldset) => {
-                  return fieldset.fields.map((field) => (
-                    <Grid.Column className="field-column" key={field}>
-                      <label
-                        htmlFor={'field-' + field}
-                        className={
-                          schema.required.includes(field) ? 'required' : ''
-                        }
-                      >
-                        {schema.properties[field].title}
-                      </label>
-
-                      <Field
-                        {...schema.properties[field]}
-                        id={field}
-                        fieldSet={fieldset.title.toLowerCase()}
-                        formData={term}
-                        focus={false}
-                        value={term[field]}
-                        required={schema.required.includes(field)}
-                        onChange={(id, value) => onChangeTerm(index, id, value)}
-                        key={field}
-                        wrapped={false}
-                        placeholder={schema.properties[field].title}
-                      />
-
-                      <p className="help">
-                        {schema.properties[field].description}
-                      </p>
-                    </Grid.Column>
-                  ));
-                })}
-                {allowDelete && (
-                  <Grid.Column width={1} className="term-actions">
-                    <Button
-                      icon="trash"
-                      negative
-                      onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        deleteTerm(index);
-                      }}
-                      className="delete-term"
-                      title={intl.formatMessage(messages.deleteTerm)}
-                      size="mini"
-                    />
-                  </Grid.Column>
-                )}
-              </Grid.Row>
-            ))}
-            {allowAppend && (
-              <Grid.Row className="bottom-buttons">
-                <Grid.Column textAlign="center">
-                  <Button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      addTerm();
-                    }}
-                    primary
-                    size="mini"
-                  >
-                    {intl.formatMessage(messages.addTerm)}
-                  </Button>
-                </Grid.Column>
-              </Grid.Row>
-            )}
-          </Grid>
+    <FormFieldWrapper {...props} wrapped={false}>
+      <Form.Field
+        inline
+        required={required}
+        error={error.length > 0}
+        className={cx(
+          description ? 'help' : '',
+          className,
+          `field-wrapper-${id}`,
+          multilingual_options?.language_independent
+            ? 'language-independent-field'
+            : null,
         )}
-      </div>
+      >
+        <Grid>
+          <Grid.Row stretched>
+            <Grid.Column width="12">
+              <div className="wrapper">
+                <label
+                  id={`fieldset-${fieldSet}-field-label-${id}`}
+                  htmlFor={noForInFieldLabel ? null : `field-${id}`}
+                >
+                  {/* <i
+                    aria-hidden="true"
+                    className="grey bars icon drag handle"
+                  /> */}
+                  {title}
+                </label>
+              </div>
+              {/* {onEdit && !isDisabled && (
+                <div className="toolbar" style={{ zIndex: '2' }}>
+                  <button
+                    aria-label={intl.formatMessage(messages.edit)}
+                    className="item ui noborder button"
+                    onClick={(evt) => {
+                      evt.preventDefault();
+                      onEdit(id);
+                    }}
+                  >
+                    <IconOld name="write square" size="large" color="blue" />
+                  </button>
+                  <button
+                    aria-label={intl.formatMessage(messages.delete)}
+                    className="item ui noborder button"
+                    onClick={(evt) => {
+                      evt.preventDefault();
+                      onDelete(id);
+                    }}
+                  >
+                    <IconOld name="close" size="large" color="red" />
+                  </button>
+                </div>
+              )} */}
+              {description && (
+                <Grid.Row stretched>
+                  <Grid.Column stretched width="12">
+                    <p className="help">
+                      {multilingual_options
+                        ? `${intl.formatMessage(
+                            messages.language_independent,
+                          )} `
+                        : null}
+                      {description}
+                    </p>
+                  </Grid.Column>
+                </Grid.Row>
+              )}
+              <div className="data-grid-widget">
+                {schema && (
+                  <>
+                    <DragDropList
+                      childList={values.map((term, index) => [
+                        index.toString(),
+                        term,
+                      ])}
+                      onMoveItem={(result) => {
+                        const { source, destination } = result;
+                        if (!destination) {
+                          return;
+                        }
+                        moveTerm(source.index, destination.index);
+                        return true;
+                      }}
+                      as={(props) => (
+                        <Grid {...props} stackable columns="equal" />
+                      )}
+                    >
+                      {({ child: term, childId, index, draginfo }) => {
+                        return (
+                          <Grid.Row key={childId} stretched>
+                            {allowReorder && (
+                              <div
+                                style={{
+                                  display: 'inline-block',
+                                }}
+                                {...draginfo.dragHandleProps}
+                                className="drag handle wrapper"
+                              >
+                                <Icon name={dragSVG} size="18px" />
+                              </div>
+                            )}
+                            {schema.fieldsets.map((fieldset) => {
+                              return fieldset.fields.map((field) => (
+                                <Grid.Column
+                                  className="field-column"
+                                  key={field}
+                                >
+                                  <label
+                                    htmlFor={'field-' + field}
+                                    className={
+                                      schema.required.includes(field)
+                                        ? 'required'
+                                        : ''
+                                    }
+                                  >
+                                    {schema.properties[field].title}
+                                  </label>
+
+                                  <Field
+                                    {...schema.properties[field]}
+                                    id={field}
+                                    fieldSet={fieldset.title.toLowerCase()}
+                                    formData={term}
+                                    focus={false}
+                                    value={term[field]}
+                                    required={schema.required.includes(field)}
+                                    onChange={(id, value) =>
+                                      onChangeTerm(index, id, value)
+                                    }
+                                    key={field}
+                                    wrapped={false}
+                                    placeholder={schema.properties[field].title}
+                                  />
+
+                                  <p className="help">
+                                    {schema.properties[field].description}
+                                  </p>
+                                </Grid.Column>
+                              ));
+                            })}
+                            {allowDelete && (
+                              <Grid.Column
+                                width={1}
+                                className="term-actions"
+                                verticalAlign="middle"
+                              >
+                                <Button
+                                  icon="trash"
+                                  negative
+                                  onClick={(e) => {
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    deleteTerm(index);
+                                  }}
+                                  className="delete-term"
+                                  title={intl.formatMessage(
+                                    messages.deleteTerm,
+                                  )}
+                                  size="mini"
+                                />
+                              </Grid.Column>
+                            )}
+                          </Grid.Row>
+                        );
+                      }}
+                    </DragDropList>
+                    {allowAppend && (
+                      <div className="bottom-buttons">
+                        <Button
+                          onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            addTerm();
+                          }}
+                          primary
+                          size="mini"
+                        >
+                          {intl.formatMessage(messages.addTerm)}
+                        </Button>
+                      </div>
+                    )}
+                  </>
+                )}
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Form.Field>
     </FormFieldWrapper>
   );
 };
